c5a957401ab2a86c2f52fb4b0c584397ba19af89
Thisiss0710 <82769462+Thisiss0710@users.noreply.github.com>
2022-03-29 04:00:29 +0800

    1

--- predict/prediction_tool.py
+++ predict/prediction_tool.py
@@ -1,11 +1,46 @@
+import math
 import numpy as np
 import pandas as pd
 import scipy.optimize as spo
+import scipy.stats as ss
+import matplotlib.pyplot as plt
 
-def prediction(stock):
-    stock = pd.read_csv(f"stock_data/{stock}.csv",
+stock = "AAPL"
+CI = 0.99
+
+data = pd.read_csv(f"stock_data/{stock}.csv",
                         index_col="Date",
                         usecols=["Date", "Adj Close"],
                         parse_dates=True)
-    expected_log_return=pd.read_csv("prediction_data/{stock}_mu.csv")
-    expected_log_return=pd.read_csv("prediction_data/{stock}_sig.csv")
+expected_log_return = pd.read_csv(f"prediction_data/{stock}_mu.csv").x
+expected_std = pd.read_csv(f"prediction_data/{stock}_sig.csv").x
+
+quantile_width = [1 - (1 - CI) ** (1 / (i + 1)) for i in range(len(expected_log_return))]
+min_quantile = (1 - np.array(quantile_width)) / 2
+max_quantile = 1 - (1 - np.array(quantile_width)) / 2
+
+min_return = [ss.norm.ppf(min_quantile[i],
+                          expected_log_return[:i + 1],
+                          expected_std[:i + 1]) for i in range(len(expected_log_return))]
+max_return = [ss.norm.ppf(max_quantile[i],
+                          expected_log_return[:i + 1],
+                          expected_std[:i + 1]) for i in range(len(expected_log_return))]
+
+    
+expected_prices = [data["Adj Close"].iloc[-1]]
+min_prices = []
+max_prices = []
+for i in range(len(expected_log_return)):
+    expected_prices.append(expected_prices[-1] * math.exp(expected_log_return[i]))
+    min_price, max_price = data["Adj Close"].iloc[-1], data["Adj Close"].iloc[-1]
+    for j in range(i + 1):
+        min_price *= math.exp(min_return[i][j - 1])
+        max_price *= math.exp(max_return[i][j - 1])
+    min_prices.append(min_price)
+    max_prices.append(max_price)
+        
+expected_prices = expected_prices[1:]
+
+plt.plot(expected_prices)
+plt.plot(min_prices)
+plt.plot(max_prices)
