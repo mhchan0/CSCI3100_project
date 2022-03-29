8ab170e5b1db8d90490e805e1a30e2059fcad0e6
him192021 <99976450+him192021@users.noreply.github.com>
2022-03-29 21:22:20 +0800

    Update csv_to_mongo.py

--- csv_to_mongo.py
+++ csv_to_mongo.py
@@ -2,11 +2,26 @@ import csv
 import pandas as pd
 import pymongo
 import json
-client = pymongo.MongoClient("fill in the blank")
+import glob
+import re
+
+client = pymongo.MongoClient("fill in the black")
 db = client['stock']
 collection = db['stockdata']
 
-data = pd.read_csv('A.csv',encoding = 'UTF-8')
-#get the info
-data_json = json.loads(data.to_json(orient='records'))
+path="stock2"
+all_files=glob.glob(path+'\*.csv')
+
+#q=collection.delete_many({})
+
+for filename in all_files:
+    nocsv=re.sub('.csv', '' ,filename)
+    nopath=re.sub(path, '', nocsv)
+
+    data = pd.read_csv(filename,encoding = 'UTF-8')
+    #get the info
+    data_json = json.loads(data.to_json(orient='records'))
+    for data in data_json:
+        data['stocknumber']=nopath
+
 collection.insert_many(data_json)
