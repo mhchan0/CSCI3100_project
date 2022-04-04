8315cd835fe5d79ec5f21a085a8190e943c2e1b3
Thisiss0710 <82769462+Thisiss0710@users.noreply.github.com>
2022-04-04 21:18:00 +0800

    Update prediction.R

--- predict/prediction.R
+++ predict/prediction.R
@@ -1,12 +1,25 @@
 library(rugarch)
 
 arma_garch_pred <- function(ticker, m=7, use_period=125, arma_order=c(1, 1), garch_order=c(2, 2)) {
-  # ticker: ticker of stock to predict
-  # m: Number of periods (days) to predict
-  # arma_order: Order of ARMA model
-  # garch_order: Order of GARCH model
-  
-  # Retrieving data from csv & calculating log return
+  # Predicts expected log return and standard deviation, and output them to csv files.
+    
+  # Parameters
+  # ----------
+  # ticker: str
+  #   ticker of stock to predict
+  # m: int, optional
+  #   Number of periods (days) to predict. The default is 7.
+  # use_period: int, optional
+  #   Number of periods (days) to use for prediction. The default is 125.
+  # arma_order: vector of int, optional
+  #   Order of ARMA part of the model. The default is c(1, 1).
+  # garch_order: vector of int, optional
+  #   Order of GARCH part of the model. The default is c(2, 2).
+
+  # Returns
+  # -------
+  # None.
+
   path <- paste("stock_data/", ticker, ".csv", sep="")
   stock <- read.csv(path)$Adj.Close
   stock <- tail(stock, use_period)
@@ -34,4 +47,4 @@ arma_garch_pred <- function(ticker, m=7, use_period=125, arma_order=c(1, 1), gar
   write.csv(mu.predict, paste("prediction_data/", ticker, "_mu.csv", sep=""))
   write.csv(sig.predict, paste("prediction_data/", ticker, "_sig.csv", sep=""))     
 }
-arma_garch_pred("1997.HK", m=20)
+arma_garch_pred("AWK", m=20)
