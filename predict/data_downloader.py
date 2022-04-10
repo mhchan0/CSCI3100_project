84956eed2f6c5181ff75736dc0bd20546e3e17e2
Thisiss0710 <82769462+Thisiss0710@users.noreply.github.com>
2022-04-10 20:51:05 +0800

    Update data_downloader.py

--- predict/data_downloader.py
+++ predict/data_downloader.py
@@ -8,9 +8,9 @@ def data_download(symbols=None, date=datetime.date(2017, 1, 1)):
 
     Parameters
     ----------
-    symbols : Nonetype or list-like, optional
+    symbols : None or string, optional
         If None, download all SP500 & HSI constituent stock data;
-        else download all stocks in the list. 
+        else stock with symbol as input. 
         The default is None.
     date : datetime.date object, optional
         Starting date of data to be downloaded. 
@@ -38,22 +38,25 @@ def data_download(symbols=None, date=datetime.date(2017, 1, 1)):
                 while stock.shape[0] < 3 and retry > 0:   # For error handling, as yfinance sometimes does not work for unknown reasons
                     stock = yf.download(symbol[0], start=modified_start, end=modified_end, timeout=3)
                     retry -= 1
-                stock.to_csv(f'stock_data/{symbol[0]}.csv')
+                if retry > 0:
+                    stock.to_csv(f'stock_data/{symbol[0]}.csv')
+                else:
+                    print(f"Error downloading data for {symbol}. Please check if inputs are correct.")
             except Exception:
                 continue
     else:
-        for symbol in symbols:
-            try:
-                stock = yf.download(symbol, start=modified_start, end=modified_end, timeout=3)
-                retry = 10
-                while stock.shape[0] < 3 and retry > 0:   # For error handling, as yfinance sometimes does not work for unknown reasons
-                    stock = yf.download(symbol, start=modified_start, end=modified_end, timeout=3)
-                    retry -= 1
-                if retry == 0:
-                    print(f"Error downloading data for {stock}. Please check if inputs are correct.")
-                stock.to_csv(f'stock_data/{symbol}.csv')
-            except Exception:
-                continue
+        try:
+            stock = yf.download(symbols, start=modified_start, end=modified_end, timeout=3)
+            retry = 10
+            while stock.shape[0] < 3 and retry > 0:   # For error handling, as yfinance sometimes does not work for unknown reasons
+                stock = yf.download(symbols, start=modified_start, end=modified_end, timeout=3)
+                retry -= 1
+            if retry > 0:
+                stock.to_csv(f'stock_data/{symbol[0]}.csv')
+            else:
+                print(f"Error downloading data for {symbols}. Please check if inputs are correct.")
+        except Exception:
+            pass
     # SP500 = yf.download('SPY',start=datetime.date(2019, 10, 2), end=modified_end)
     # SP500.to_csv(f'stock_data/SP500.csv')
 
